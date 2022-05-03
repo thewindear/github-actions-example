add91c42e0836b30936aeb8b5be042b6a529e4c1
thewindear <thewindear@outlook.com>
2022-05-03 15:45:54 +0800

    [learn]学习env关键字

--- tests/util_test.go
+++ tests/util_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
     "github-actions-example/utils"
+    "os"
     "testing"
 )
 
@@ -9,5 +10,14 @@ func TestUtil_Random(t *testing.T) {
     randStr := utils.RandomStr(10)
     if len(randStr) != 10 {
         t.Error("random string number not 10")
+    }
+}
+
+func TestUtil_LookEnv(t *testing.T) {
+    if os.Getenv("GO_USERNAME") != "root" {
+        t.Error("GO_USERNAME NOT ROOT")
+    }
+    if os.Getenv("GO_PASSWORD") != "root" {
+        t.Error("GO_PASSWORD NOT ROOT")
     }
 }
